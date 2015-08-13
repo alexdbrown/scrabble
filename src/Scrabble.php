d676db8f4939b39ae27110c756c8f31921b9a68a
Austin Blanchard & alexandra brown <student@epicodus.com>
2015-08-13 15:09:40 -0700

    Add real word input verification

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -3,26 +3,38 @@
     {
         function scrabbleScore($input)
         {
+
+            $dictionary = (file_get_contents("https://docs.oracle.com/javase/tutorial/collections/interfaces/examples/dictionary.txt"));
+            $new_dictionary = explode("\n", $dictionary);
+
             // Lower case user word
             $word = strtolower($input);
 
-            // Split up user word into individual letters
-            $letters_in_array = str_split($word);
+            if (!in_array($word, $new_dictionary)) {
+                return "Sorry, that's not a real word. Nigel says try again.";
+            } else {
 
-            // Assign letter values
-            $alphabet = array("a" => 1, "b" => 3, "c" => 3, "d" => 2, "e" => 1,
-            "f" => 4, "g" => 2, "h" => 4, "i" => 1, "j" => 8, "k" => 5, "l" => 1,
-            "m" => 3, "n" => 1, "o" => 1, "p" => 3, "q" => 10, "r" => 1, "s" => 1,
-            "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8, "y" => 4, "z" => 10);
+                // Split up user word into individual letters
+                $letters_in_array = str_split($word);
 
-            $score = 0;
+                // Assign letter values
+                $alphabet = array("a" => 1, "b" => 3, "c" => 3, "d" => 2, "e" => 1,
+                "f" => 4, "g" => 2, "h" => 4, "i" => 1, "j" => 8, "k" => 5, "l" => 1,
+                "m" => 3, "n" => 1, "o" => 1, "p" => 3, "q" => 10, "r" => 1, "s" => 1,
+                "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8, "y" => 4, "z" => 10);
 
-            foreach ($letters_in_array as $single_letter) {
-                $score += ($alphabet[$single_letter]);
+                $score = 0;
+
+                foreach ($letters_in_array as $single_letter) {
+                    $score += ($alphabet[$single_letter]);
+                }
+
             }
 
             return $score;
+
         }
+
     }
 
  ?>
